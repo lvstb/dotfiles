8e47067f120539c8c0cd05a34c24def52448b154
lvstb <lars@wingu.dev>
2022-06-01 22:52:46 +0200

    schemastore enabled

--- config/nvim/lua/plugins/lsp-installer.lua
+++ config/nvim/lua/plugins/lsp-installer.lua
@@ -24,11 +24,12 @@ lsp_installer.on_server_ready(function(server)
                 },
                 hover = true,
                 completion = true,
-
+                validate = true,
                 customTags = {
                     "!fn",
                     "!And",
                     "!If",
+                    "!If Sequence",
                     "!Not",
                     "!Equals",
                     "!Equals Scalar",
@@ -47,6 +48,21 @@ lsp_installer.on_server_ready(function(server)
                     "!Split",
                     "!Join sequence"
                     },
+                schemaStore = {
+                    url = "https://www.schemastore.org/api/json/catalog.json",
+                    enable = true,
+                },
+                schemas = {
+                    ["https://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.ya?ml",
+                    ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+                    ["https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json"] = "*cf*.ya?ml",
+                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.ya?ml",
+                    ["https://json.schemastore.org/prettierrc.json"] = ".prettierrc.ya?ml",
+                    ["https://json.schemastore.org/eslintrc.json"] = ".eslintrc.ya?ml",
+                    ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.ya?ml",
+                    ["https://yarnpkg.com/configuration/yarnrc.json"] = ".yarnrc.ya?ml",
+                    ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.ya?ml",
+                },
             },
         }
 	end
