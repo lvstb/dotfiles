06e93e3e598f0f794c760925e9c0f5a3c24aa0ff
lvstb <lars@wingu.dev>
2022-12-04 21:15:24 +0100

    hupse

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,39 +1,42 @@
-
 local actions = require("telescope.actions")
+local fb_actions = require("telescope").extensions.file_browser.actions
+
 require("telescope").setup({
-    defaults = {
-        file_sorter = require("telescope.sorters").get_fzy_sorter,
-        prompt_prefix = " >",
-        color_devicons = true,
+	defaults = {
+		file_sorter = require("telescope.sorters").get_fzy_sorter,
+		prompt_prefix = " >",
+		color_devicons = true,
 
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
-        mappings = {
-            i = {
-                ["<C-x>"] = false,
-                ["<C-q>"] = actions.send_to_qflist,
-            },
-        },
-    },
-    extensions = {
-      fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-        },
-    file_browser = {
-      theme = "ivy",
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
-      },
-    },
-    },
+		mappings = {
+			i = {
+				["<C-x>"] = false,
+				["<C-q>"] = actions.send_to_qflist,
+			},
+		},
+	},
+	extensions = {
+		fzy_native = {
+			override_generic_sorter = false,
+			override_file_sorter = true,
+		},
+		file_browser = { -- disables netrw and use telescope-file-browser in its place
+			hijack_netrw = true,
+			-- theme = "ivy",
+			mappings = {
+				["i"] = {
+					-- your custom insert mode mappactionsings
+					["<C-a>"] = fb_actions.create_from_prompt,
+					["<C-r>"] = fb_actions.rename,
+					["<C-m>"] = fb_actions.move,
+				},
+				["n"] = {},
+			},
+		},
+	},
 })
 require("telescope").load_extension("git_worktree")
 require("telescope").load_extension("fzy_native")
@@ -41,24 +44,24 @@ require("telescope").load_extension("file_browser")
 
 local M = {}
 M.search_dotfiles = function()
-    require("telescope.builtin").find_files({
-        prompt_title = "< VimRC >",
-        cwd = vim.env.DOTFILES,
-        hidden = true,
-    })
+	require("telescope.builtin").find_files({
+		prompt_title = "< VimRC >",
+		cwd = vim.env.DOTFILES,
+		hidden = true,
+	})
 end
 
-require "telescope.builtin".lsp_definitions { jump_type = "vsplit" }
-require "telescope.builtin".lsp_type_definitions { jump_type = "vpslit" }
+require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
+require("telescope.builtin").lsp_type_definitions({ jump_type = "vpslit" })
 
 M.git_branches = function()
-    require("telescope.builtin").git_branches({
-        attach_mappings = function(_, map)
-            map("i", "<c-d>", actions.git_delete_branch)
-            map("n", "<c-d>", actions.git_delete_branch)
-            return true
-        end,
-    })
+	require("telescope.builtin").git_branches({
+		attach_mappings = function(_, map)
+			map("i", "<c-d>", actions.git_delete_branch)
+			map("n", "<c-d>", actions.git_delete_branch)
+			return true
+		end,
+	})
 end
 
 return M
