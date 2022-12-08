4a6b92746e2520aacafae002008c83dc2a70ed9a
lvstb <lars@wingu.dev>
2022-12-08 22:22:38 +0100

    tmux sessions

--- config/nvim/lua/core/init.lua
+++ config/nvim/lua/core/init.lua
@@ -1,37 +1,38 @@
-local global = require "core.global"
+local global = require("core.global")
 local vim = vim
 
 -- Create cache dir and subs dir
 local createdir = function()
-    local data_dir = {
-        global.cache_dir .. "backup", global.cache_dir .. "session",
-        global.cache_dir .. "swap", global.cache_dir .. "tags",
-        global.cache_dir .. "undo"
-    }
-    -- There only check once that If cache_dir exists
-    -- Then I don't want to check subs dir exists
-    if vim.fn.isdirectory(global.cache_dir) == 0 then
-        os.execute("mkdir -p " .. global.cache_dir)
-        for _, v in pairs(data_dir) do
-            if vim.fn.isdirectory(v) == 0 then
-                os.execute("mkdir -p " .. v)
-            end
-        end
-    end
+	local data_dir = {
+		global.cache_dir .. "backup",
+		global.cache_dir .. "session",
+		global.cache_dir .. "swap",
+		global.cache_dir .. "tags",
+		global.cache_dir .. "undo",
+	}
+	-- There only check once that If cache_dir exists
+	-- Then I don't want to check subs dir exists
+	if vim.fn.isdirectory(global.cache_dir) == 0 then
+		os.execute("mkdir -p " .. global.cache_dir)
+		for _, v in pairs(data_dir) do
+			if vim.fn.isdirectory(v) == 0 then
+				os.execute("mkdir -p " .. v)
+			end
+		end
+	end
 end
 
 local load_core = function()
-    createdir()
-    -- leader_map()
+	createdir()
+	-- leader_map()
 
-    require("core.options")
-    require ("core.theme")
-    require ("core.augroup")
-    --require("core.event")
-    --Include autocommands in vimscript
-    vim.cmd 'source $HOME/.config/nvim/lua/core/autocommands.vim'
-    -- vim.cmd [[colorscheme onenord]]
-
+	require("core.options")
+	require("core.theme")
+	require("core.augroup")
+	require("core.disablebuiltins")
+	--Include autocommands in vimscript
+	vim.cmd("source $HOME/.config/nvim/lua/core/autocommands.vim")
+	-- vim.cmd [[colorscheme onenord]]
 end
 
 load_core()
